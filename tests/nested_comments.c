96abb4875403ee29bd152cba785b78d1089c2b52
Joel Grunbaum <joelgrun@gmail.com>
2019-09-22 21:53:08 +0100

    added escape codes and nested comment tests

---
+++ tests/nested_comments.c
@@ -0,0 +1,16 @@
+//
+////
+///////
+//// this is a test // \n //
+/* lets see how this works 
+ * sdfsd
+ *
+ *
+ */
+
+    int main(void) {
+    	printf("Hello world!\n\n\b\f\0");
+	char c = 'c';
+	c = '\\';
+	c = '\n';
+}
