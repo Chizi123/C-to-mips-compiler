2d2e5d2e1e9e891ae78a6a70cdd5d65d2f419549
Joel Grunbaum <joelgrun@gmail.com>
2019-09-30 11:44:32 +0100

    Added some movement. Assignement eats next character in lexer

--- tests/struct.c
+++ tests/struct.c
@@ -3,6 +3,7 @@ struct test
     int i;
     int j;
     char *str;
+    char *a[5];
 };
 
 int main ()
@@ -11,4 +12,9 @@ int main ()
 	aaa.i = 3;
 	aaa.j = 4;
 	strcpy("Hello", aaa.str);
+	aaa.i = 4;
+	aaa.a = 'c';
+
+	return aaa.i;
+	return aaa.j;
 }
